b30ef7623d37d389f4a50cee07e44eec0a6ac7b3
Tanul Bhasin <7824529+smartaquarius10@users.noreply.github.com>
2023-01-20 16:28:48 +0530

    Create root.go

---
+++ cmd/cobrainit/root.go
@@ -0,0 +1,40 @@
+package cobrainit
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/smartaquarius10/kubecli/cmd/contextauth"
+	"github.com/smartaquarius10/kubecli/cmd/objects/deployments"
+	"github.com/smartaquarius10/kubecli/cmd/objects/pod"
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "kubectl command",
+		Short: "Commands to manage kubernetes",
+		Long:  `Kubectl is necessary on the machine`,
+	}
+)
+
+func Execute() {
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(contextauth.SwapCmd)
+	rootCmd.AddCommand(contextauth.DeleteCmd)
+	rootCmd.AddCommand(deployments.BackupCmd())
+	rootCmd.AddCommand(deployments.ApplyCmd())
+	rootCmd.AddCommand(deployments.RemoveCmd())
+	rootCmd.AddCommand(deployments.ViewCmd())
+	rootCmd.AddCommand(deployments.ScaleCmd())
+	rootCmd.AddCommand(deployments.SelectorCmd())
+	rootCmd.AddCommand(pod.DeleteCmd())
+	//rootCmd.AddCommand(pod.LoginCmd())
+}
