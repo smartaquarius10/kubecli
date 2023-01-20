95ac6920e9641b5ffa7dc954716d442e8a22a34a
Tanul Bhasin <7824529+smartaquarius10@users.noreply.github.com>
2023-01-20 16:31:52 +0530

    Create delete.go

---
+++ objects/pod/delete.go
@@ -0,0 +1,30 @@
+package pod
+
+import (
+	"fmt"
+
+	"github.com/smartaquarius10/kubecli/cmd"
+	"github.com/smartaquarius10/kubecli/cmd/objects"
+	"github.com/spf13/cobra"
+)
+
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete pod in a namespace",
+	Run: func(cmd *cobra.Command, args []string) {
+		namespace, _ := cmd.Flags().GetString("namespace")
+		deletepod(namespace)
+	},
+}
+
+func DeleteCmd() *cobra.Command {
+	deleteCmd.Flags().StringP("namespace", "n", "", "Pass namespace having deployments")
+	deleteCmd.MarkFlagRequired("namespace")
+	return deleteCmd
+}
+
+func deletepod(namespace string) {
+	podname := objects.SelectObject(namespace, "pods", "pod/")
+	stdout := cmd.ExecuteCommand("delete", "pods", podname, "-n", namespace)
+	fmt.Println(string(stdout))
+}
